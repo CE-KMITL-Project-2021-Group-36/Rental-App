0070a8134bb35ae24685a2c2abfe41030f0b5810
Watcharakorn <watcharakorn.yew@gmail.com>
2021-11-05 04:54:08 +0700

    [feat] Create navbar

---
+++ rental_app/lib/navigation_bar.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+class NavigationBar extends StatefulWidget {
+  const NavigationBar({Key? key}) : super(key: key);
+
+  @override
+  _NavigationBarState createState() => _NavigationBarState();
+}
+
+class _NavigationBarState extends State<NavigationBar> {
+  int _currentIndex = 0;
+
+  //Test pages
+  final pages = [
+    const Center(child: Text('หน้าแรก')),
+    const Center(child: Text('ข้อความ')),
+    const Center(child: Text('รถเข็น')),
+    const Center(child: Text('แจ้งเตือน')),
+    const Center(child: Text('บัญชี')),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: pages[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: Theme.of(context).primaryColor,
+        selectedFontSize: 12,
+        unselectedFontSize: 12,
+        type: BottomNavigationBarType.fixed,
+        currentIndex: _currentIndex,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        items:  const <BottomNavigationBarItem>[
+           BottomNavigationBarItem(
+            icon: Icon(Icons.home_outlined),
+            activeIcon: Icon(Icons.home),
+            label: 'หน้าแรก',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.chat_bubble_outline),
+            activeIcon: Icon(Icons.chat_bubble),
+            label: 'ข้อความ',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_cart_outlined),
+            activeIcon: Icon(Icons.shopping_cart),
+            label: 'รถเข็น',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.notifications_outlined),
+            activeIcon: Icon(Icons.notifications),
+            label: 'แจ้งเตือน',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person_outlined),
+            activeIcon: Icon(Icons.person),
+            label: 'บัญชี',
+          ),
+        ],
+      ),
+    );
+  }
+}
