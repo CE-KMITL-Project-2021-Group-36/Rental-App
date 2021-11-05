c30eba30ad3f62a85ddad06fca80e8b577066841
Watcharakorn <watcharakorn.yew@gmail.com>
2021-11-05 08:11:14 +0700

    [feat] Update grid view categories

--- rental_app/lib/home_page.dart
+++ rental_app/lib/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './category_card.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -21,7 +22,70 @@ class _HomePageState extends State<HomePage> {
         ),
         backgroundColor: Colors.white,
       ),
-      body: Container(),
+      body: Container(
+        child: Column(
+          children: [
+            Container(
+                // padding: EdgeInsets.all(16.0),
+                child: Column(
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: TextButton(
+                        onPressed: () => {},
+                        style: ButtonStyle(
+                            shape:
+                                MaterialStateProperty.all<RoundedRectangleBorder>(
+                                    RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.circular(24.0),
+                                        side: BorderSide(color: Colors.grey)))),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            Text("ค้นหาสินค้า",
+                                style: TextStyle(color: Colors.grey)),
+                            Icon(Icons.search),
+                          ],
+                        ),
+                      ),
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 16.0),
+                      child: Align(
+                        alignment: Alignment.centerLeft,
+                        child: Text("หมวดหมู่แนะนำ",
+                            style: TextStyle(fontWeight: FontWeight.bold)),
+                      ),
+                    ),
+                    SafeArea(
+                        child: SizedBox(
+                      width: double.infinity,
+                      height: 300,
+                      child: GridView(
+                        padding: EdgeInsets.all(8),
+                        scrollDirection: Axis.horizontal,
+                        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+                            maxCrossAxisExtent: 250,
+                            childAspectRatio: 4 / 3,
+                            crossAxisSpacing: 8,
+                            mainAxisSpacing: 8),
+                        children: [
+                          CategoryCard(),
+                          CategoryCard(),
+                          CategoryCard(),
+                          CategoryCard(),
+                          CategoryCard(),
+                          CategoryCard(),
+                          CategoryCard(),
+                          
+                        ],
+                      ),
+                    )),
+                  ],
+                )),
+          ],
+        ),
+      ),
     );
   }
 }
