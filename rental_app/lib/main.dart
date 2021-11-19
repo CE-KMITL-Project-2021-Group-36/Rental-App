c7983789b6169f0cc61cdf602fc7e0dc0791a7b5
Puriwat <savad.dee555@gmail.com>
2021-11-19 21:53:20 +0700

    [feat] Create login page

--- rental_app/lib/main.dart
+++ rental_app/lib/main.dart
@@ -1,21 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:rental_app/core/app_widget.dart';
 import './navigation_bar.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Test Flutter',
-      theme: ThemeData(
-        primarySwatch: Colors.deepPurple,
-      ),
-      home: const Navigation_Bar(),
-    );
-  }
+  runApp(const AppWidget());
 }
