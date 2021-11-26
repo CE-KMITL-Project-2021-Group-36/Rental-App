1234d3c5ce09b6927a9b11c7597b9bab2384dac1
Watcharakorn Thankaew <watcharakorn.yew@gmail.com>
2021-11-26 19:22:45 +0700

    [feat] Add products card

--- rental_app/lib/home_page.dart
+++ rental_app/lib/home_page.dart
@@ -8,14 +8,6 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  final List<Map> myCategories = List.generate(
-      24,
-      (index) => {
-            "id": index,
-            "name": "ชนิด $index",
-            "image": "https://picsum.photos/250?image=$index"
-          }).toList();
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -64,7 +56,7 @@ class _HomePageState extends State<HomePage> {
                       ),
                       Text(
                         "ดูทั้งหมด",
-                        style: TextStyle(color: Colors.indigo),
+                        style: TextStyle(fontSize: 12, color: Colors.indigo),
                       ),
                     ]),
               ),
@@ -78,9 +70,12 @@ class _HomePageState extends State<HomePage> {
                         "https://assets.ajio.com/medias/sys_master/root/20210403/OjjF/6068dc44aeb269a9e33a52ba/-288Wx360H-462103975-pink-MODEL.jpg"),
                     _buildCategoryProduct("เสื้อผ้าผู้หญิง",
                         "https://image.freepik.com/free-photo/stunning-curly-female-model-jumping-purple-indoor-portrait-slim-girl-bright-yellow-dress_197531-10836.jpg"),
-                    _buildCategoryProduct("กล้อง และอุปกรณ์", "https://image.freepik.com/free-photo/photographer-snapping-with-analog-camera_53876-105703.jpg"),
-                    _buildCategoryProduct("ตั้งแคมป์", "https://image.freepik.com/free-photo/group-man-woman-enjoy-camping-picnic-barbecue-lake-with-tents-background-young-mixed-race-asian-woman-man-young-people-s-hands-toasting-cheering-bottles-beer_1253-1041.jpg"),
-                    _buildCategoryProduct("หนังสือ", "https://image.freepik.com/free-photo/book-library-with-open-textbook_1150-5924.jpg"),
+                    _buildCategoryProduct("กล้อง และอุปกรณ์",
+                        "https://image.freepik.com/free-photo/photographer-snapping-with-analog-camera_53876-105703.jpg"),
+                    _buildCategoryProduct("ตั้งแคมป์",
+                        "https://image.freepik.com/free-photo/group-man-woman-enjoy-camping-picnic-barbecue-lake-with-tents-background-young-mixed-race-asian-woman-man-young-people-s-hands-toasting-cheering-bottles-beer_1253-1041.jpg"),
+                    _buildCategoryProduct("หนังสือ",
+                        "https://image.freepik.com/free-photo/book-library-with-open-textbook_1150-5924.jpg"),
                   ],
                 ),
               ),
@@ -95,10 +90,27 @@ class _HomePageState extends State<HomePage> {
                       ),
                       Text(
                         "ดูทั้งหมด",
-                        style: TextStyle(color: Colors.indigo),
+                        style: TextStyle(fontSize: 12, color: Colors.indigo),
                       ),
                     ]),
               ),
+              const SizedBox(
+                height: 8,
+              ),
+              Container(
+                margin: const EdgeInsets.symmetric(horizontal: 16),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    _buildItemCard(
+                        'กล้อง Canon EOS พร้อมเลนส์ ให้เช่าราคาถูก',
+                        '฿100 /ชม.\n฿700 /วัน.\n฿4500 /สัปดาห์.\n',
+                        'https://www.ec-mall.com/wp-content/uploads/2018/03/eos-1500d_02-1.jpg'),
+                    _buildItemCard('กล้อง Instax กล้องฟิล์ม', '฿600 /วัน.',
+                        'https://m.media-amazon.com/images/I/71HmLp20ovL._AC_SL1500_.jpg'),
+                  ],
+                ),
+              )
             ],
           ),
         ));
@@ -125,6 +137,73 @@ class _HomePageState extends State<HomePage> {
                 style: TextStyle(fontSize: 12),
               ))
         ],
+      ),
+    );
+  }
+
+  Widget _buildItemCard(String name, String price, String imageUrl) {
+    return Card(
+      clipBehavior: Clip.antiAlias,
+      child: InkWell(
+        onTap: () {},
+        child: Container(
+          width: 164,
+          height: 300,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                height: 164,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    fit: BoxFit.cover,
+                    image: NetworkImage(imageUrl),
+                  ),
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  children: [
+                    Text(
+                      name,
+                      //'กล้อง Canon EOS พร้อมเลนส์ ให้เช่าราคาถูก',
+                      style: TextStyle(
+                        fontSize: 12,
+                        //fontStyle: FontStylefontWeight: FontWeight.bold,
+                      ),
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                      textAlign: TextAlign.left,
+                    ),
+                    Container(
+                      padding: const EdgeInsets.symmetric(vertical: 4),
+                      child: Row(children: [
+                        Icon(Icons.star, color: Colors.yellow[600], size: 16),
+                        Icon(Icons.star, color: Colors.yellow[600], size: 16),
+                        Icon(Icons.star, color: Colors.yellow[600], size: 16),
+                        Icon(Icons.star, color: Colors.yellow[600], size: 16),
+                        Icon(Icons.star, color: Colors.yellow[600], size: 16),
+                      ]),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        //'฿100 /ชม.\n฿700 /วัน.\n฿4500 /สัปดาห์.\n',
+                        price,
+                        style: TextStyle(fontSize: 12, color: Colors.indigo
+                            //fontStyle: FontStylefontWeight: FontWeight.bold,
+                            ),
+                        maxLines: 3,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              //Image.asset('assets/card-sample-image-2.jpg'),
+            ],
+          ),
+        ),
       ),
     );
   }
