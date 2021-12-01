d3141c5b53538a8ccec2b88bf82624bcbeff994a
Puriwat <savad.dee555@gmail.com>
2021-12-02 02:11:23 +0700

    [feat] Implement Injectable and Get It

--- rental_app/lib/main.dart
+++ rental_app/lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:injectable/injectable.dart';
+import 'package:rental_app/injection.dart';
 import 'package:rental_app/presentation/core/app_widget.dart';
 
 import 'presentation/bottom_nav_bar.dart';
 
 void main() {
+  configureInjection(Environment.prod);
   runApp(const AppWidget());
 }
