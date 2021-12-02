74df51f3dbf7393143497c8c7063483fc21db22f
Puriwat <savad.dee555@gmail.com>
2021-12-02 16:54:25 +0700

    [test] Running firebase on iOS

--- rental_app/lib/main.dart
+++ rental_app/lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 import 'bottom_nav_bar.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -12,14 +15,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-
       debugShowCheckedModeBanner: false,
       title: 'Test Flutter',
       theme: ThemeData(
         primarySwatch: Colors.indigo,
       ),
       home: const BottomNavBar(),
-
     );
   }
 }
