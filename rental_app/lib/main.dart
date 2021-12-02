cfc127f6767cb60827901d6446de6991f441d248
Watcharakorn Thankaew <watcharakorn.yew@gmail.com>
2021-12-03 04:14:16 +0700

    [feat] Create notification page

--- rental_app/lib/main.dart
+++ rental_app/lib/main.dart
@@ -4,8 +4,8 @@ import 'package:firebase_core/firebase_core.dart';
 import 'bottom_nav_bar.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  // WidgetsFlutterBinding.ensureInitialized();
+  // await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
