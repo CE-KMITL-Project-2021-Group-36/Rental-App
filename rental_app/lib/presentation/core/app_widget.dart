5987b9e550e199e487da6dd3b933bdc18f75fe5e
Puriwat <savad.dee555@gmail.com>
2021-12-03 01:27:45 +0700

    [style] Finish login UI

--- rental_app/lib/presentation/core/app_widget.dart
+++ rental_app/lib/presentation/core/app_widget.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:rental_app/presentation/bottom_nav_bar.dart';
-import 'package:rental_app/presentation/sign_in//sign_in_page.dart';
 
 class AppWidget extends StatelessWidget {
   const AppWidget({Key? key}) : super(key: key);
@@ -11,8 +10,28 @@ class AppWidget extends StatelessWidget {
       title: 'Rental App',
       theme: ThemeData(
         primarySwatch: Colors.deepPurple,
+        inputDecorationTheme: InputDecorationTheme(
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+        ),
+        textButtonTheme: TextButtonThemeData(
+          style: TextButton.styleFrom(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+          ),
+        ),
+        outlinedButtonTheme: OutlinedButtonThemeData(
+          style: OutlinedButton.styleFrom(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+          ),
+        ),
       ),
       home: const BottomNavBar(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
--- rental_app/lib/presentation/sign_in/widgets/sign_in_form.dart
+++ rental_app/lib/presentation/sign_in/widgets/sign_in_form.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:rental_app/application/auth/sign_in_form/sign_in_form_bloc.dart';
 
 class SignInForm extends StatelessWidget {
@@ -11,14 +12,103 @@ class SignInForm extends StatelessWidget {
       listener: (context, state) {},
       builder: (context, state) {
         return Form(
-          child: ListView(
-            children: const [
-              Text(
-                'T',
-                textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 130),
-              ),
-            ],
+          child: Padding(
+            padding: const EdgeInsets.all(20),
+            child: ListView(
+              children: [
+                Container(
+                  margin: const EdgeInsets.symmetric(vertical: 50),
+                  child: const Text(
+                    'ลงชื่อเข้าใช้',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 36,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.deepPurple,
+                    ),
+                  ),
+                ),
+                TextFormField(
+                  decoration: const InputDecoration(
+                    prefixIcon: Icon(Icons.email),
+                    labelText: 'อีเมล',
+                  ),
+                  autocorrect: false,
+                ),
+                const SizedBox(height: 10),
+                TextFormField(
+                  decoration: const InputDecoration(
+                    prefixIcon: Icon(Icons.lock),
+                    labelText: 'รหัสผ่าน',
+                  ),
+                  autocorrect: false,
+                  obscureText: true,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    TextButton(
+                      onPressed: () {},
+                      child: const Text(
+                        'ลืมรหัสผ่าน?',
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 10),
+                Expanded(
+                  child: TextButton(
+                    onPressed: () {},
+                    child: const Text('ลงชื่อเข้าใช้'),
+                    style: TextButton.styleFrom(
+                      primary: Colors.white,
+                      backgroundColor: Colors.deepPurple,
+                      padding: const EdgeInsets.symmetric(vertical: 15),
+                      textStyle: const TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 30),
+                Expanded(
+                  child: OutlinedButton.icon(
+                    onPressed: () {},
+                    icon: const Icon(FontAwesomeIcons.google),
+                    label: const Text('ดำเนินการต่อด้วย Google'),
+                    style: TextButton.styleFrom(
+                      primary: Colors.black,
+                      backgroundColor: Colors.white,
+                      padding: const EdgeInsets.symmetric(vertical: 15),
+                      textStyle: const TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 30),
+                const Text(
+                  'หรือ',
+                  style: TextStyle(fontSize: 18),
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(height: 30),
+                TextButton(
+                  onPressed: () {},
+                  child: const Text('สมัครสมาชิกด้วยอีเมล'),
+                  style: TextButton.styleFrom(
+                    alignment: Alignment.center,
+                    textStyle: const TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                      decoration: TextDecoration.underline,
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         );
       },
