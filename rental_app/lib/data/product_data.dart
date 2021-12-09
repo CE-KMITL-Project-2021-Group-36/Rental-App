60eef03cd04cd634dd1e3c2d82ccc4c9a4efc5c8
Watcharakorn Thankaew <watcharakorn.yew@gmail.com>
2021-12-09 07:56:13 +0700

    [feat] Create search filter

---
+++ rental_app/lib/data/product_data.dart
@@ -0,0 +1,17 @@
+import 'package:rental_app/model/product.dart';
+
+final allProducts = <Product>[
+  Product(
+    id: 1,
+    name: "กล้อง Canon EOS พร้อมเลนส์ ให้เช่าราคาถูก",
+    price: "฿100 /ชม.\n฿700 /วัน.\n฿4500 /สัปดาห์.",
+    imageUrl:
+        "https://www.ec-mall.com/wp-content/uploads/2018/03/eos-1500d_02-1.jpg",
+  ),
+  Product(
+    id: 2,
+    name: "กล้อง Instax กล้องฟิล์ม",
+    price: "฿600 /วัน.",
+    imageUrl: "https://m.media-amazon.com/images/I/71HmLp20ovL._AC_SL1500_.jpg",
+  ),
+];
