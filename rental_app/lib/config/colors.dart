83c1e9f2d50c371552f916584ef131ae10a97b77
Watcharakorn Thankaew <watcharakorn.yew@gmail.com>
2022-01-16 14:24:45 +0700

    [style] Update color theme

---
+++ rental_app/lib/config/colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:rental_app/config/palette.dart';
+
+const primaryColor = Palette.bluePurple;
+const primaryLightColor = Color(0xffF0EEFF);
+const secondaryColor = Color(0xff0ACF97);
+const textColor = Color(0xff212121);
+const surfaceColor = Color(0xffFFFFFF);
+const backgroundColor = Color(0xffFBFBFF);
+const errorColor = Color(0xffFF3D48);
+const warningdColor = Color(0xffFBBC05);
+const successdColor = Color(0xff3AFFA1);
--- rental_app/lib/config/theme.dart
+++ rental_app/lib/config/theme.dart
@@ -1,64 +1,58 @@
 import 'package:flutter/material.dart';
-import 'package:rental_app/config/palette.dart';
-
-const primaryColor = Palette.bluePurple;
-const primaryLightColor = Color(0xffF0EEFF);
-const secondaryColor = Color(0xff0ACF97);
-const textColor = Color(0xff212121);
-const surfaceColor = Color(0xffFFFFFF);
-const backgroundColor = Color(0xffFBFBFF);
+import 'package:rental_app/config/colors.dart';
 
 ThemeData theme() {
   return ThemeData(
+    primaryColor: primaryColor,
     primarySwatch: primaryColor,
     scaffoldBackgroundColor: backgroundColor,
-    fontFamily: "",
-    textTheme: textTheme(),
+    fontFamily: "Roboto",
+    //textTheme: textTheme(),
   );
 }
 
-TextTheme textTheme() {
-  return const TextTheme(
-    headline1: TextStyle(
-      color: Colors.black,
-      fontWeight: FontWeight.bold,
-      fontSize: 36,
-    ),
-    headline2: TextStyle(
-      color: Colors.black,
-      fontWeight: FontWeight.bold,
-      fontSize: 24,
-    ),
-    headline3: TextStyle(
-      color: Colors.black,
-      fontWeight: FontWeight.bold,
-      fontSize: 18,
-    ),
-    headline4: TextStyle(
-      color: Colors.black,
-      fontWeight: FontWeight.bold,
-      fontSize: 16,
-    ),
-    headline5: TextStyle(
-      color: Colors.black,
-      fontWeight: FontWeight.bold,
-      fontSize: 14,
-    ),
-    headline6: TextStyle(
-      color: Colors.black,
-      fontWeight: FontWeight.normal,
-      fontSize: 14,
-    ),
-    bodyText1: TextStyle(
-      color: Colors.black,
-      fontWeight: FontWeight.normal,
-      height: 1.75,
-      fontSize: 12,
-    ),
-    bodyText2: TextStyle(
-      color: Colors.black,
-      fontWeight: FontWeight.normal,
-      fontSize: 10,
-    ),
-  );
-}
+// TextTheme textTheme() {
+//   return const TextTheme(
+//     headline1: TextStyle(
+//       color: textColor,
+//       fontWeight: FontWeight.bold,
+//       fontSize: 36,
+//     ),
+//     headline2: TextStyle(
+//       color: textColor,
+//       fontWeight: FontWeight.bold,
+//       fontSize: 24,
+//     ),
+//     headline3: TextStyle(
+//       color: textColor,
+//       fontWeight: FontWeight.bold,
+//       fontSize: 18,
+//     ),
+//     headline4: TextStyle(
+//       color: textColor,
+//       fontWeight: FontWeight.bold,
+//       fontSize: 16,
+//     ),
+//     headline5: TextStyle(
+//       color: textColor,
+//       fontWeight: FontWeight.bold,
+//       fontSize: 14,
+//     ),
+//     headline6: TextStyle(
+//       color: textColor,
+//       fontWeight: FontWeight.normal,
+//       fontSize: 14,
+//     ),
+//     bodyText1: TextStyle(
+//       color: textColor,
+//       fontWeight: FontWeight.normal,
+//       height: 1.75,
+//       fontSize: 12,
+//     ),
+//     bodyText2: TextStyle(
+//       color: textColor,
+//       fontWeight: FontWeight.normal,
+//       fontSize: 10,
+//     ),
+//   );
+// }
