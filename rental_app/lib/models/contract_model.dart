576ada2ac842424c60243f2920ce3e9357bd02a6
Watcharakorn Thankaew <watcharakorn.yew@gmail.com>
2022-03-28 18:13:21 +0700

    [feat] Add Contract Model

---
+++ rental_app/lib/models/contract_model.dart
@@ -0,0 +1,37 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class Contract {
+  final String productId;
+  final String renterId;
+  final Timestamp startDate;
+  final Timestamp endDate;
+  final double deposit;
+  final double rentalPrice;
+  final String status;
+  final List<String> imageUrls;
+
+  const Contract({
+    required this.productId,
+    required this.renterId,
+    required this.startDate,
+    required this.endDate,
+    required this.deposit,
+    required this.rentalPrice,
+    required this.status,
+    required this.imageUrls,
+  });
+
+  static Contract fromSnapshot(DocumentSnapshot snapshot) {
+    Contract contract = Contract(
+      productId: snapshot['productId'],
+      renterId: snapshot['renterId'],
+      startDate: snapshot['startDate'],
+      endDate: snapshot['endDate'],
+      deposit: snapshot['deposit'],
+      rentalPrice: snapshot['rentalPrice'],
+      status: snapshot['status'],
+      imageUrls: snapshot['imageUrls'],
+    );
+    return contract;
+  }
+}
