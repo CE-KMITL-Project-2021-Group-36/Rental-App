a30088dbbf6a5ee52091394124ac599ab914ed93
Puriwat <savad.dee555@gmail.com>
2022-04-13 15:16:18 +0700

    [feat] [chore] Add form validation on top up screen and update package

--- rental_app/lib/screens/wallet_top_up.dart
+++ rental_app/lib/screens/wallet_top_up.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:keyboard_dismisser/keyboard_dismisser.dart';
 import 'package:rental_app/config/palette.dart';
@@ -23,12 +24,23 @@ class WalletTopUp extends StatefulWidget {
 }
 
 class _WalletTopUpState extends State<WalletTopUp> {
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     final userId = FirebaseAuth.instance.currentUser!.uid;
     final CollectionReference users =
         FirebaseFirestore.instance.collection('users');
     final TextEditingController _amount = TextEditingController();
+    ValueNotifier<bool> _continuousValidation = ValueNotifier(false);
+
+    Future<void> _onPressedFunction() async {
+      if (!_formKey.currentState!.validate()) {
+        _continuousValidation.value = true;
+      } else {
+        debugPrint('Submitting ' + _amount.text);
+      }
+    }
 
     return KeyboardDismisser(
       gestures: const [GestureType.onTap, GestureType.onPanUpdateAnyDirection],
@@ -44,26 +56,27 @@ class _WalletTopUpState extends State<WalletTopUp> {
         body: SafeArea(
           child: Padding(
             padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-            child: FutureBuilder<DocumentSnapshot>(
-                future: users.doc(userId).get(),
-                builder: (context, snapshot) {
-                  if (snapshot.hasError) {
-                    return const Text("ข้อมูลผิดพลาด");
-                  }
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  children: [
+                    FutureBuilder<DocumentSnapshot>(
+                        future: users.doc(userId).get(),
+                        builder: (context, snapshot) {
+                          if (snapshot.hasError) {
+                            return const Text("ข้อมูลผิดพลาด");
+                          }
 
-                  if (snapshot.hasData && !snapshot.data!.exists) {
-                    return const Text("ไม่พบข้อมูล");
-                  }
-                  if (snapshot.connectionState == ConnectionState.done) {
-                    Map<String, dynamic> data =
-                        snapshot.data!.data() as Map<String, dynamic>;
-                    final double? balance = data['wallet']?['balance'];
-                    return Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Column(
-                          children: [
-                            Container(
+                          if (snapshot.hasData && !snapshot.data!.exists) {
+                            return const Text("ไม่พบข้อมูล");
+                          }
+                          if (snapshot.connectionState ==
+                              ConnectionState.done) {
+                            Map<String, dynamic> data =
+                                snapshot.data!.data() as Map<String, dynamic>;
+                            final double? balance = data['wallet']?['balance'];
+                            return Container(
                               width: double.infinity,
                               height: 100,
                               decoration: BoxDecoration(
@@ -106,54 +119,79 @@ class _WalletTopUpState extends State<WalletTopUp> {
                                   ),
                                 ],
                               ),
-                            ),
-                            const SizedBox(
-                              height: 20,
-                            ),
-                            TextFormField(
+                            );
+                          }
+                          return const Center(
+                            child: CircularProgressIndicator(),
+                          );
+                        }),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    ValueListenableBuilder(
+                        valueListenable: _continuousValidation,
+                        builder:
+                            (BuildContext context, bool val, Widget? child) {
+                          return Form(
+                            key: _formKey,
+                            autovalidateMode: _continuousValidation.value
+                                ? AutovalidateMode.onUserInteraction
+                                : AutovalidateMode.disabled,
+                            child: TextFormField(
                               controller: _amount,
                               decoration: const InputDecoration(
+                                prefixText: '฿',
                                 labelText: 'จำนวนเงิน',
+                                helperText: 'ระบุจำนวนเงิน 1 - 100,000 บาท',
                               ),
                               autocorrect: false,
                               keyboardType: TextInputType.number,
+                              inputFormatters: [
+                                LengthLimitingTextInputFormatter(6)
+                              ],
                               validator: (input) {
-                                if (input!.isEmpty) return 'โปรดระบุจำนวนเงิน';
+                                if (!RegExp(
+                                        r"""^([1-9]|[1-8][0-9]|9[0-9]|[1-8][0-9]{2}|9[0-8][0-9]|99[0-9]|[1-8][0-9]{3}|9[0-8][0-9]{2}|99[0-8][0-9]|999[0-9]|[1-8][0-9]{4}|9[0-8][0-9]{3}|99[0-8][0-9]{2}|999[0-8][0-9]|9999[0-9]|100000)$""")
+                                    .hasMatch(input!)) {
+                                  return 'ระบุจำนวนเงิน 1 - 100,000 บาท เท่านั้น';
+                                } else {}
                                 return null;
                               },
                             ),
-                          ],
-                        ),
-                        Container(
-                          child: Row(
-                            children: [
-                              Expanded(
-                                child: TextButton(
-                                  child: Text(
-                                    'ดำเนินการต่อ',
-                                    style: textTheme().button,
-                                  ),
-                                  style: TextButton.styleFrom(
-                                    primary: Colors.white,
-                                    backgroundColor: _amount != null
-                                        ? primaryColor
-                                        : primaryLightColor,
-                                    padding: const EdgeInsets.symmetric(
-                                        vertical: 15),
-                                  ),
-                                  onPressed: () {},
+                          );
+                        }),
+                  ],
+                ),
+                ValueListenableBuilder<TextEditingValue>(
+                    valueListenable: _amount,
+                    builder: (context, value, child) {
+                      return Row(
+                        children: [
+                          Expanded(
+                            child: TextButton(
+                              child: Text(
+                                'ดำเนินการต่อ',
+                                style: textTheme().button,
+                              ),
+                              style: TextButton.styleFrom(
+                                primary: Colors.white,
+                                backgroundColor: value.text.isNotEmpty
+                                    ? primaryColor
+                                    : primaryLightColor,
+                                padding: const EdgeInsets.symmetric(
+                                  vertical: 15,
                                 ),
                               ),
-                            ],
+                              onPressed: value.text.isNotEmpty
+                                  ? _onPressedFunction
+                                  : null,
+                            ),
                           ),
-                        ),
-                      ],
-                    );
-                  }
-                  return const Center(
-                    child: CircularProgressIndicator(),
-                  );
-                }),
+                        ],
+                      );
+                    })
+              ],
+            ),
           ),
         ),
       ),
