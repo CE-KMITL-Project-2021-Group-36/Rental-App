a3aa70a864b3b585009ddcc0bb55376611129b08
Puriwat <savad.dee555@gmail.com>
2022-04-16 19:08:49 +0700

    [style] Currency format

--- rental_app/lib/screens/wallet.dart
+++ rental_app/lib/screens/wallet.dart
@@ -3,6 +3,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:rental_app/config/palette.dart';
+import 'package:rental_app/config/theme.dart';
 
 class Wallet extends StatefulWidget {
   const Wallet({Key? key}) : super(key: key);
@@ -89,7 +90,7 @@ class _WalletState extends State<Wallet> {
                               bottom: 10,
                               right: 10,
                               child: Text(
-                                '฿ $balance',
+                                '฿' + currencyFormat(balance!),
                                 style: const TextStyle(
                                   fontSize: 36,
                                   fontWeight: FontWeight.w600,
