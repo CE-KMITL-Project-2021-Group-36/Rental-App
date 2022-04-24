46472614eab43c87fe79548ec6ab0c577c85472c
Watcharakorn Thankaew <watcharakorn.yew@gmail.com>
2022-04-25 02:49:34 +0700

    [fix] Fix major code

--- rental_app/lib/screens/notification_screen.dart
+++ rental_app/lib/screens/notification_screen.dart
@@ -1,12 +1,17 @@
+import 'dart:convert';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:rental_app/config/palette.dart';
+import 'package:http/http.dart' as http;
 
 class NotificationScreen extends StatefulWidget {
-  const NotificationScreen({ Key? key }) : super(key: key);
+  const NotificationScreen({Key? key}) : super(key: key);
 
   @override
   _NotificationScreenState createState() => _NotificationScreenState();
-  
+
   static const String routeName = '/notification';
 
   static Route route() {
@@ -18,6 +23,8 @@ class NotificationScreen extends StatefulWidget {
 }
 
 class _NotificationScreenState extends State<NotificationScreen> {
+  final currentUserId = FirebaseAuth.instance.currentUser!.uid;
+
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
@@ -29,7 +36,6 @@ class _NotificationScreenState extends State<NotificationScreen> {
             'แจ้งเตือน',
             style: TextStyle(fontWeight: FontWeight.bold),
           ),
-
           elevation: 2,
           bottom: const TabBar(
             labelColor: primaryColor,
@@ -45,48 +51,241 @@ class _NotificationScreenState extends State<NotificationScreen> {
         ),
         body: TabBarView(
           children: [
-            SingleChildScrollView(
-              child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    _buildNotificationCard(Icons.timer,'ระยะเวลาเช่าใกล้หมดแล้ว','รายการ: กล้อง Canon EOS พร้อมเลนส์ ให้เช่าราคาถูก กำลังจะหมดระยะเวลาเช่าใน 3 วัน',),
-                    _buildNotificationCard(Icons.local_shipping,'สินค้าส่งถึงแล้ว','รายการ: กล้อง Canon EOS พร้อมเลนส์ ให้เช่าราคาถูก สินค้าถูกจัดส่งแล้ว กรุณาตรวจสอบสินค้า',),
-                    _buildNotificationCard(Icons.local_shipping,'สินค้ากำลังถูกจัดส่ง','รายการ: กล้อง Canon EOS พร้อมเลนส์ ให้เช่าราคาถูก สินค้ากำลังจัดส่งโดยไปรษณีย์ไทย',),
-                    _buildNotificationCard(Icons.local_shipping,'ร้านค้ากำลังเตรียมจัดส่ง','รายการ: กล้อง Canon EOS พร้อมเลนส์ ให้เช่าราคาถูก ร้าน RentKlong รับคำขอเช่าแล้วกำลังเตรียมจัดส่ง',),
-                  ]),
-            ),
-            SingleChildScrollView(
-              child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    _buildNotificationCard(Icons.timer,'ระยะเวลาเช่าหมดแล้ว','รายการ: เสื้อเชิ้ทสีกรม กำลังหมดระยะเวลาเช่าแล้วกำลังรอการคืนของจากผู้เช่า',),
-                  ]),
-            ),
+            _buildRenterNotificationList(),
+            _buildOwnerNotificationList()
           ],
         ),
       ),
     );
   }
 
-  Widget _buildNotificationCard(icon, title, subtitle,) {
-    return Card(
-      elevation: 0,
-      shape: RoundedRectangleBorder(
-    borderRadius: BorderRadius.circular(16.0),
-  ),
-      child: Container(
-        padding: const EdgeInsets.all(8),
-        child: ListTile(
-          horizontalTitleGap: 2,
-          leading: Icon(icon, color: primaryColor),
-          title: Text(title),
-          subtitle: Text(
-            subtitle,
-            maxLines: 2,
-            style: TextStyle(fontSize: 12,color: Colors.black.withOpacity(0.6)),
+  Widget _buildRenterNotificationList() {
+    return StreamBuilder<QuerySnapshot>(
+      stream: FirebaseFirestore.instance
+          .collection('users')
+          .doc(currentUserId)
+          .collection('notifications')
+          .where('type', isEqualTo: 'renter')
+          .orderBy('createdOn', descending: true)
+          .snapshots(),
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return const Text('มีบางอย่างผิดพลาด');
+        }
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(
+            child: Padding(
+              padding: EdgeInsets.all(16.0),
+              child: CircularProgressIndicator(),
+            ),
+          );
+        }
+        final data = snapshot.data;
+        return data?.size == 0
+            ? const Padding(
+                padding: EdgeInsets.all(16.0),
+                child: Center(child: Text('ไม่มีการแจ้งเตือน')),
+              )
+            : ListView.builder(
+                physics: const BouncingScrollPhysics(),
+                shrinkWrap: true,
+                itemCount: data!.docs.length,
+                itemBuilder: (context, index) {
+                  final doc = data.docs[index];
+                  return _buildNotificationWithIcon(
+                    doc['title'],
+                    doc['text'],
+                    doc['type'],
+                  );
+                },
+              );
+      },
+    );
+  }
+
+  Widget _buildOwnerNotificationList() {
+    return StreamBuilder<QuerySnapshot>(
+      stream: FirebaseFirestore.instance
+          .collection('users')
+          .doc(currentUserId)
+          .collection('notifications')
+          .where('type', isEqualTo: 'owner')
+          .orderBy('createdOn', descending: true)
+          .snapshots(),
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return const Text('มีบางอย่างผิดพลาด');
+        }
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(
+            child: Padding(
+              padding: EdgeInsets.all(16.0),
+              child: CircularProgressIndicator(),
+            ),
+          );
+        }
+        final data = snapshot.data;
+        return data?.size == 0
+            ? const Padding(
+                padding: EdgeInsets.all(16.0),
+                child: Center(child: Text('ไม่มีการแจ้งเตือน')),
+              )
+            : ListView.builder(
+                physics: const BouncingScrollPhysics(),
+                shrinkWrap: true,
+                itemCount: data!.docs.length,
+                itemBuilder: (context, index) {
+                  final doc = data.docs[index];
+                  return _buildNotificationWithIcon(
+                    doc['title'],
+                    doc['text'],
+                    doc['type'],
+                  );
+                },
+              );
+      },
+    );
+  }
+
+  _buildNotificationWithIcon(title, text, type) {
+    switch (title) {
+      case 'กรุณาชำระค่าเช่า':
+        return _buildNotificationCard(
+          icon: Icons.paid,
+          title: title,
+          subtitle: text,
+          type: type,
+        );
+      case 'สินค้าจัดส่งแล้ว':
+        return _buildNotificationCard(
+          icon: Icons.local_shipping,
+          title: title,
+          subtitle: text,
+          type: type,
+        );
+      case 'การเช่าสำเร็จ':
+        return _buildNotificationCard(
+          icon: Icons.verified,
+          title: title,
+          subtitle: text,
+          type: type,
+        );
+      case 'มีคำขอเช่าใหม่':
+        return _buildNotificationCard(
+          icon: Icons.mail,
+          title: title,
+          subtitle: text,
+          type: type,
+        );
+      case 'ผู้เช่าได้รับสินค้าแล้ว':
+        return _buildNotificationCard(
+          icon: Icons.done,
+          title: title,
+          subtitle: text,
+          type: type,
+        );
+      case 'ผู้เช่าจัดส่งสินค้าแล้ว':
+        return _buildNotificationCard(
+          icon: Icons.local_shipping,
+          title: title,
+          subtitle: text,
+          type: type,
+        );
+    }
+  }
+
+  Widget _buildNotificationCard({icon, title, subtitle, type}) {
+    return GestureDetector(
+      onTap: () {
+        type == 'renter'
+            ? Navigator.pushNamed(context, '/contract_management',
+                arguments: 'renter')
+            : Navigator.pushNamed(context, '/contract_management',
+                arguments: 'owner');
+      },
+      child: Card(
+        elevation: 0,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(16.0),
+        ),
+        child: Container(
+          padding: const EdgeInsets.all(8),
+          child: ListTile(
+            horizontalTitleGap: 2,
+            leading: Icon(icon, color: primaryColor),
+            title: Text(
+              title,
+              style: const TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+                color: textColor,
+              ),
+            ),
+            subtitle: Text(
+              subtitle,
+              maxLines: 2,
+              style: const TextStyle(
+                fontSize: 12,
+                color: Colors.grey,
+              ),
+            ),
           ),
-        )
+        ),
       ),
     );
+  }
+}
+
+void sendNotification(receiver, title, text, type) async {
+  CollectionReference notification = FirebaseFirestore.instance
+      .collection('users')
+      .doc(receiver)
+      .collection('notifications');
+
+  String fcmToken = '';
+  var docSnapshot =
+      await FirebaseFirestore.instance.collection('users').doc(receiver).get();
+  if (docSnapshot.exists) {
+    Map<String, dynamic>? data = docSnapshot.data();
+    fcmToken = data?['fcmToken'];
+    if (fcmToken != '') {
+      sendPushNotification(title, text, fcmToken);
+    }
+  }
+
+  if (type == 'chat') return;
+
+  await notification.add({
+    'createdOn': DateTime.now(),
+    'title': title,
+    'text': text,
+    'type': type,
+  });
+}
+
+void sendPushNotification(title, text, fcmToken) async {
+  try {
+    await http.post(
+      Uri.parse('https://fcm.googleapis.com/fcm/send'),
+      headers: <String, String>{
+        'Content-Type': 'application/json',
+        'Authorization':
+            'key=AAAAC7m5wqY:APA91bGNW85s5iGBQPZZufehmMz-WvyXvzwzuYxItcd16klNcu4pOr-_uEcjeCDTt45no9AMwB7tyLoAm-EZCK9f4m1KiRU7ZCz1Fj918ytzIHx9-ftsqUxTbopD-5Yr-dmYNZxHv6Iu',
+      },
+      body: jsonEncode(
+        <String, dynamic>{
+          'notification': <String, dynamic>{'body': text, 'title': title},
+          'priority': 'high',
+          'data': <String, dynamic>{
+            'click_action': 'FLUTTER_NOTIFICATION_CLICK',
+            'id': '1',
+            'status': 'done'
+          },
+          "to": fcmToken,
+        },
+      ),
+    );
+  } catch (e) {
+    debugPrint("error push notification");
   }
 }
